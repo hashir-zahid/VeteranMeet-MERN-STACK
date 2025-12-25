3d81346106cc082f6f2ff760ff969f32be5c1d79
Hashir <hashir.logic@gmail.com>
2025-12-25 14:19:54 +0500

    Initial commit for services

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,1 +1,22 @@
-export const signin = () => {};
+// services/authService.js
+import api from "./api";
+
+// Veteran Registration
+export const registerVeteran = (data) => {
+  return api.post("/auth/register/veteran", data);
+};
+
+// Organization Registration
+export const registerOrganization = (data) => {
+  return api.post("/auth/register/organization", data);
+};
+
+// Login (Veteran / Organization)
+export const loginUser = (data) => {
+  return api.post("/auth/login", data);
+};
+
+// Logout
+export const logout = () => {
+  localStorage.removeItem("token");
+};
